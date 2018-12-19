1562f8c53c0eeb25d93b7134bc524bf5629cde2d
Tim Fielder <fiel1139@gmail.com>
2018-12-18 21:11:30 -0700

    Creates template for mentors controllers.

---
+++ app/controllers/api/v1/admin/mentors_controller.rb
@@ -0,0 +1,15 @@
+class MentorsController < ApplicationController
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/api/v1/mentors_controller.rb
@@ -0,0 +1,15 @@
+class MentorsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+end
