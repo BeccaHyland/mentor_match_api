10e89fbf56576155992ea871c33c2ef9ef6caab6
Tim Fielder <fiel1139@gmail.com>
2018-12-19 15:28:21 -0700

    Creates factory for mentor.

--- spec/factories/mentors.rb
+++ spec/factories/mentors.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :mentor do
     name {Faker::TheITCrowd.character}
-    email {Faker::TheITCrowd.email}
+    email {Faker::Internet.email}
     city { "Denver" }
     state { "CO" }
     slack_username { Faker::Cat.name }
     linkedin_username { "linkedin_username" }
     github_username { "github_username" }
-    matched { Faker::Boolean.boolean }
+    matched { true }
   end
 end
