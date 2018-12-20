ac607bead8ce0f82de41c65913183a384bc48a51
Tim Fielder <fiel1139@gmail.com>
2018-12-19 19:43:42 -0700

    Creates test for individual mentor request. Implements mentor show action for json response.

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::MentorsController < ApplicationController
   end
 
   def show
-
+    id = params[:id]
+    render json: MentorSerializer.new(Mentor.find_by_id(id))
   end
 
   def update
---
+++ spec/requests/api/v1/mentor_request_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'mentors API' do
+  describe 'GET /api/v1/mentors/:id' do
+    it 'returns a specific mentor from db' do
+    mentor_1, mentor_2, mentor_3 = create_list(:mentor, 3)
+
+      #payload = { }
+
+      get '/api/v1/mentors/1'
+
+      expect(response.status).to eq(200)
+
+      get_response = JSON.parse(response.body, symbolize_names: true)
+      # expect(get_response[:data]).to be_a(Array)
+      expect(get_response[:data]).to be_a(Object)
+      # expect(get_response[:data].length).to eq(1)
+
+      expect(get_response[:data][:attributes][:name]).to eq(mentor_1[:name])
+      expect(get_response[:data][:attributes][:email]).to eq(mentor_1[:email])
+      expect(get_response[:data][:attributes][:city]).to eq(mentor_1[:city])
+      expect(get_response[:data][:attributes][:state]).to eq(mentor_1[:state])
+      expect(get_response[:data][:attributes][:slack_username]).to eq(mentor_1[:slack_username])
+      expect(get_response[:data][:attributes][:linkedin_username]).to eq(mentor_1[:linkedin_username])
+      expect(get_response[:data][:attributes][:github_username]).to eq(mentor_1[:github_username])
+      expect(get_response[:data][:attributes][:matched]).to eq(mentor_1[:matched])
+      expect(get_response[:data][:attributes][:active]).to eq(mentor_1[:active])
+      expect(get_response[:data][:attributes][:program]).to eq(mentor_1[:program])
+
+    end
+  end
+end
