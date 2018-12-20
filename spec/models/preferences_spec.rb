1aebf9f1ed97b93ba75b7624f5593d483f5f0b67
Tim Fielder <fiel1139@gmail.com>
2018-12-19 18:54:05 -0700

    Adds preferences_spec.rb to models in specs.

---
+++ spec/models/preferences_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Preference, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of (:title)}
+  end
+  it 'exists' do
+    preference = Preference.new()
+    expect(preference).to be_a(Preference)
+  end
+  it 'has attributes' do
+    title = "parent"
+
+    attributes = {
+      title: "parent"
+    }
+
+    preference = Preference.new(attributes)
+
+    expect(preference.title).to eq(title)
+  end
+end
