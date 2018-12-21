6a04045b4a7b6c1cf61ffc6ee648504dc766a5cd
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-20 17:29:30 -0700

    run migration, pushing this up to test whether travis will perform first build

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2018_12_20_020517) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+  end
+
   add_foreign_key "mentor_preferences", "mentors"
   add_foreign_key "mentor_preferences", "preferences"
 end
