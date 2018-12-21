1e92b8e8f3e0c0ec2e7ac1edace4bd0a48742f7b
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-21 15:11:54 -0700

    update GET mentor request to reflect new data shape from serializer (after adding a mentor's preferences to the serializer)

--- spec/requests/api/v1/mentor_request_spec.rb
+++ spec/requests/api/v1/mentor_request_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 describe 'mentors API' do
   describe 'GET /api/v1/mentors/:id' do
     it 'returns a specific mentor from db' do
-    mentor_1, mentor_2, mentor_3 = create_list(:mentor, 3)
+      mentor_1 = Mentor.create(name: "test mentor 1", email: "test@email.com", city: "Test City", state: "CO", slack_username: "test1", linkedin_username: "test1", github_username: "test1", program: "BE")
+      mentor_2 = Mentor.create(name: "test mentor 2", email: "test@contact.com", city: "Test Town", state: "CO", slack_username: "test2", linkedin_username: "test2", github_username: "test2", program: "FE")
+
+      mentor_1.preferences.create!(title: "veteran")
+      mentor_1.preferences.create!(title: "female-identifying")
+      mentor_2.preferences.create!(title: "parent")
 
       id = mentor_1.id
       #payload = { }
@@ -25,6 +30,7 @@ describe 'mentors API' do
       expect(get_response[:data][:attributes][:matched]).to eq(mentor_1[:matched])
       expect(get_response[:data][:attributes][:active]).to eq(mentor_1[:active])
       expect(get_response[:data][:attributes][:program]).to eq(mentor_1[:program])
+      expect(get_response[:data][:attributes][:preferences].first[:title]).to eq(mentor_1.preferences.first.title)
 
     end
   end
