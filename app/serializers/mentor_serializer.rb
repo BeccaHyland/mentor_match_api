47ccd03e494b15b8eac859fd9410ab3c74094cad
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-21 14:49:19 -0700

    draft factories, may not end up using, not currently able to implement 'through' association in factory bot

--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -1,4 +1,5 @@
 class MentorSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :email, :city, :state, :slack_username, :linkedin_username, :github_username, :program, :active, :matched
+  has_many :preferences
 end
---
+++ app/serializers/preference_serializer.rb
@@ -0,0 +1,4 @@
+class PreferenceSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :title
+end
