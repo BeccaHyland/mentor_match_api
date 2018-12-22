7da69a318d869778f234b90d16802a2e3c48cdd9
Tim Fielder <fiel1139@gmail.com>
2018-12-22 16:47:38 -0700

    Implementation of Knock within application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  include Knock::Authenticable
+
+  protected
+
+  def authorize_as_admin
+    return_unauthorized unless !current_user.nil? && current_user.is_admin?
+  end
 end
