af48b46d60774b1c21e5dab4231c2d5b8b540ce4
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-22 14:12:04 -0700

    add mentor attributes to serializer

--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -1,6 +1,24 @@
 class MentorSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :email, :city, :state, :slack_username, :active, :matched
+  attributes :id,
+              :name,
+              :email,
+              :city,
+              :state,
+              :country,
+              :slack_username,
+              :matched,
+              :active,
+              :pronouns,
+              :current_title,
+              :current_employer,
+              :background,
+              :industries,
+              :ways_to_mentor,
+              :expertise_tech,
+              :expertise_non_tech,
+              :mentee_capacity,
+              :meeting_location
 
   attribute :preferences do |mentor|
     mentor.preferences.map do |preference|
