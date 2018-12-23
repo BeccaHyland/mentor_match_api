e2d933120a95eb6ee6d179df91a5172ffebe2655
Tim Fielder <fiel1139@gmail.com>
2018-12-22 17:13:53 -0700

    Creates routes for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  get 'auth'            => 'home#auth'
+  post 'user_token'     => 'user_token#create'
+
+  get '/users'          => 'users#index'
+  get '/users/current'  => 'users#current'
+  post '/users/create'  => 'users#create'
+  patch '/users/:id'    => 'users#update'
+  delete '/users/:id'   => 'users#destroy'
 
   namespace :api do
     namespace :v1 do
