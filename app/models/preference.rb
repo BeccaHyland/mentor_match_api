64e5eadc494fed45b5ffa237fb7eacd485fa13ad
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-24 11:47:25 -0700

    delete spec and models for preference model and join model

--- app/models/preference.rb
+++
@@ -1,6 +0,0 @@
-class Preference < ApplicationRecord
-  validates_presence_of :title
-
-  has_many :mentor_preferences
-  has_many :mentors, through: :mentor_preferences
-end
--- spec/models/mentor_preferences_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MentorPreference, type: :model do
-  describe 'relationships' do
-    it {should belong_to(:preference)}
-    it {should belong_to(:mentor)}
-  end
-end
