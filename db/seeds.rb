3607c0c2d37457df9a8a7e8468f7dfd543cddfc8
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-24 11:49:30 -0700

    remove destroy actions from seeds for preferece and join

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-MentorPreference.destroy_all
-Preference.destroy_all
 Mentor.destroy_all
 
 if Rails.env == 'development' || Rails.env == 'production'
