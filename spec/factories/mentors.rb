2f23c15bc80dd84f04fcf628087ae5fbb644f3af
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-12-24 11:27:47 -0700

    update factory with all atributes, some values left empty strings / arrays for now

--- spec/factories/mentors.rb
+++ spec/factories/mentors.rb
@@ -7,5 +7,18 @@ FactoryBot.define do
     slack_username { "slack_username" }
     matched { false }
     active { true }
+    current_title {""}
+    current_employer {""}
+    background {""}
+    industries { [] }
+    ways_to_mentor { [] }
+    expertise_tech { [] }
+    expertise_non_tech { [] }
+    stack_preference { [] }
+    identity_preference { [] }
+    mentee_capacity { 1 }
+    meeting_location {""}
+    country {""}
+
   end
 end
