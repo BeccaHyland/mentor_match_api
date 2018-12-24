ca58330351f379ac64adf2f466269cec2ea78001
Tim Fielder <fiel1139@gmail.com>
2018-12-24 11:15:34 -0700

    Serializes stack_preference for mentor.

--- app/serializers/mentor_serializer.rb
+++ app/serializers/mentor_serializer.rb
@@ -18,7 +18,8 @@ class MentorSerializer
               :expertise_tech,
               :expertise_non_tech,
               :mentee_capacity,
-              :meeting_location
+              :meeting_location,
+              :stack_preference
 
   attribute :preferences do |mentor|
     mentor.preferences.map do |preference|
