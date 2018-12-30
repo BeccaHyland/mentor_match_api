3eb87016fa5a44a57cd1f38c43d05e4ad701b0c5
Tim Fielder <fiel1139@gmail.com>
2018-12-30 10:40:09 -0700

    Creates student model.

---
+++ app/models/student.rb
@@ -0,0 +1,8 @@
+class Student < ApplicationRecord
+  validates_presence_of :name
+
+  validates :email, uniqueness: true, presence: true
+
+  has_many :student_mentors,
+  has_many :mentors, through: :student_mentors
+end
