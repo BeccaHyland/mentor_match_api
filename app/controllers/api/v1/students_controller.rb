584e7fa645786c685ec351f42518aa82a77bbc0e
Tim Fielder <fiel1139@gmail.com>
2019-01-01 10:52:40 -0700

    Creates controller for students.

---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,46 @@
+class Api::V1::StudentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    mentor = Student.new(student_params)
+    if student.save
+      render json: StudentSerializer.new(student), status: 201
+    else
+      render json: {}, status: 401
+    end
+  end
+
+  def index
+    render json: StudentSerializer.new(Student.all)
+  end
+
+  def show
+    id = params[:id]
+    render json: StudentSerializer.new(Student.find(id))
+  end
+
+  def update
+    id = params[:id]
+    Student.find(id).update(student_params)
+    render json: StudentSerializer.new(Student.find(id))
+  end
+
+  def destroy
+    id = params[:id]
+    Student.find(id).delete
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:name,
+                                    :pronouns,
+                                    :email,
+                                    :slack_username,
+                                    :matched,
+                                    :active,
+                                    :background,
+                                    industries: [],
+                                    :stack,
+                                    identity_marker: []
+    end
+end
