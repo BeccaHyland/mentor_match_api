c7eb19823703c8600126e47ee8f5461f8a64f48d
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-01-01 09:34:18 -0700

    Add test to DELETE mentor request to expect that non-admin cannot delete (a non-admin such as a mentor would instead update their own 'active' attribute to 'false' on the edit profile page,in this way admin retains record of all past/present mentors

--- spec/requests/api/v1/mentor_delete_request_spec.rb
+++ spec/requests/api/v1/mentor_delete_request_spec.rb
@@ -4,23 +4,46 @@ include Tokenator
 
 describe 'mentor API endpoints' do
   describe 'DELETE request to /api/v1/mentors/:id' do
-    it 'deletes a specific mentor from db' do
-      user = User.create(name: "name_of_user", login: "xyz")
-      token = Tokenator.encode(user.login)
+    describe 'as an admin user' do
+      it 'deletes a specific mentor from db' do
+        user = User.create(name: "name_of_user", login: "xyz", role: "admin")
+        token = Tokenator.encode(user.login)
 
-      mentor_1 = create :mentor
+        mentor_1 = create :mentor
 
-      id = mentor_1.id
-      mentor_count = Mentor.count
-      expect(Mentor.find(id)).to be_valid
+        id = mentor_1.id
+        mentor_count = Mentor.count
+        expect(Mentor.find(id)).to be_valid
 
-      delete "/api/v1/mentors/#{id}", params: {
-        token: token
-      }
+        delete "/api/v1/mentors/#{id}", params: {
+          token: token
+        }
 
-      expect(response.status).to eq(204)
-      new_mentor_count = mentor_count - 1
-      expect(Mentor.count).to eq(new_mentor_count)
+        expect(response.status).to eq(204)
+        new_mentor_count = mentor_count - 1
+        expect(Mentor.count).to eq(new_mentor_count)
+      end
+    end
+
+    describe 'as a non-admin user' do
+      it 'returns a 401 unauthorized error' do
+        user = User.create(name: "name_of_user", login: "xyz", role: "user")
+        token = Tokenator.encode(user.login)
+
+        mentor_1 = create :mentor
+
+        id = mentor_1.id
+        mentor_count = Mentor.count
+        expect(Mentor.find(id)).to be_valid
+
+        delete "/api/v1/mentors/#{id}", params: {
+          token: token
+        }
+
+        expect(response.status).to eq(401)
+        new_mentor_count = mentor_count - 1
+        expect(Mentor.count).to eq(mentor_count)
+      end
     end
   end
 end
