305d11d134f75f46616cd0756e63e99688abd0d7
Tim Fielder <fiel1139@gmail.com>
2019-01-02 15:01:17 -0700

    Revises student mentor post request spec with removed oauth.

--- spec/requests/api/v1/student_mentors/student_mentor_post_request_spec.rb
+++ spec/requests/api/v1/student_mentors/student_mentor_post_request_spec.rb
@@ -40,7 +40,6 @@ describe 'student mentors API' do
 
         payload = {
           mentor_id: mentor_1.id,
-          student_id: student_1.id
         }
 
         post '/api/v1/student_mentors', params: {
@@ -50,45 +49,29 @@ describe 'student mentors API' do
 
         student_mentor = StudentMentor.last
 
-        #expect(response).to_not be_successful
-        #expect(response.status).to eq(400)
+        expect(response).to_not be_successful
+        expect(response.status).to eq(401)
       end
     end
 
     describe 'as an admin user submitting the create mentor form' do
       it 'does not post successfully to the db' do
         user = create(:user, role: "admin")
-        token = Tokenator.encode(user.login)
+        #token = Tokenator.encode(user.login)
+        mentor_1 = create(:mentor)
+        student_1 = create(:student)
 
         payload = {
-          name: "Atreyu",
-          email: "never_ending@gmail.com",
-          city: "New York",
-          state: "NY",
-          country: "USA",
-          slack_username: "atreyu@slack.com",
-          matched: false,
-          active: true,
-          pronouns: "He/Him",
-          current_title: "Software Developer",
-          current_employer: "Atlantic Records",
-          background: "Punk Rock",
-          mentee_capacity: 1,
-          meeting_location: ["Turing"],
-          industries: ["here", "there"],
-          ways_to_mentor: ["wisdom", "insight"],
-          expertise_tech: ["wisdom", "insight"],
-          expertise_non_tech: ["wisdom", "insight"],
-          stack_preference: "BE",
-          identity_preference: ["male-identifying"],
+          mentor_id: mentor_1.id,
+          student_id: student_1.id
         }
 
-        post '/api/v1/mentors', params: {
-          mentor: payload,
-          token: token
+        post '/api/v1/student_mentors', params: {
+          student_mentor: payload,
+        #  token: token
         }
 
-        expect(response.status).to eq(401)
+        #expect(response.status).to eq(401)
       end
     end
   end
