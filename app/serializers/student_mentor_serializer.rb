142d3eda1caa63ca3059a6dde26a7084edb86301
Tim Fielder <fiel1139@gmail.com>
2019-01-02 12:41:00 -0700

    Adds notes for mentor and student serializers to StudentMentorSerializer.

--- app/serializers/student_mentor_serializer.rb
+++ app/serializers/student_mentor_serializer.rb
@@ -3,4 +3,7 @@ class StudentMentorSerializer
   attributes :student_id,
              :mentor_id,
              :active
+
+#  has_one :mentor, serializer MentorSerializer
+#  has_one :student, serializer StudentSerializer
 end
