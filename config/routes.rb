2b3f65cd4671cea8e6da63ffc09c8324cd670c99
Tim Fielder <fiel1139@gmail.com>
2019-01-01 18:11:39 -0700

    Change routing to student_mentors.

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :mentors, only: [:create, :index, :show, :update, :destroy]
       resources :students, only: [:create, :index, :show, :update, :destroy]
-
-      resources :mentors do
-        resources :students, controller: 'student_mentors', only: [:create, :index, :show, :update, :destroy]
-      end
+      resources :student_mentors, only: [:create, :index, :show, :update, :destroy]
     end
   end
 end
