82da24c5049002b7e12632f9f565506c89c74af8
Tim Fielder <fiel1139@gmail.com>
2019-01-02 16:23:53 -0700

    Revises mentors controller to include user_id.

--- app/controllers/api/v1/mentors_controller.rb
+++ app/controllers/api/v1/mentors_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::MentorsController < ApplicationController
     #   render json: {}, status: 401
     # else
       mentor = Mentor.new(mentor_params)
-      mentor.user_id = @current_user.id
+      #mentor.user_id = @current_user.id
       if mentor.save
         render json: MentorSerializer.new(mentor), status: 201
       else
@@ -71,6 +71,7 @@ class Api::V1::MentorsController < ApplicationController
                                   :background,
                                   :mentee_capacity,
                                   :stack_preference,
+                                  :user_id,
                                   industries: [],
                                   ways_to_mentor: [],
                                   expertise_tech: [],
