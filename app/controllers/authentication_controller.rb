014251f4cf8eac941d47af80325e3476c7f4d843
Tim Fielder <fiel1139@gmail.com>
2019-01-02 16:44:39 -0700

    Hibernates the authentication controller.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,21 +4,21 @@ class AuthenticationController < ApplicationController
   include Tokenator
 
   def create
-    authenticator = Authenticator.new
-    user_info = authenticator.github(params[:code])
-
-    login = user_info[:login]
-    name = user_info[:name]
-    avatar_url = user_info[:avatar_url]
-
-    token = Tokenator.encode(login)
-    User.where(login: login).first_or_create!(
-      name: name,
-      avatar_url: avatar_url
-    )
-    redirect_to "#{issuer}?token=#{token}"
-  rescue StandardError => error
-    redirect_to "#{issuer}?error=#{error.message}"
+    # authenticator = Authenticator.new
+    # user_info = authenticator.github(params[:code])
+    #
+    # login = user_info[:login]
+    # name = user_info[:name]
+    # avatar_url = user_info[:avatar_url]
+    #
+    # token = Tokenator.encode(login)
+    # User.where(login: login).first_or_create!(
+    #   name: name,
+    #   avatar_url: avatar_url
+    # )
+    # redirect_to "#{issuer}?token=#{token}"
+    # rescue StandardError => error
+    # redirect_to "#{issuer}?error=#{error.message}"
   end
 
   private
