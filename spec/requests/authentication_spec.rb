ba1f6a819864388c3ed2e34c295e39bd0a9b3e1d
BeccaHyland <rebecca.e.hyland@gmail.com>
2019-01-03 18:23:26 -0700

    add route expectation to auth request spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,12 +1,12 @@
-# require 'rails_helper'
-# require './lib/tokenator.rb'
-# include Tokenator
-#
-# describe 'Authenticator Requests' do
-#   it 'has route to /auth/github' do
-#
-#     '/auth/github'
-#
-#     expect(response).to be_successful
-#   end
-# end
+require 'rails_helper'
+require './lib/tokenator.rb'
+include Tokenator
+
+describe 'Authenticator Requests' do
+  it 'has route to /auth/github' do
+
+    get '/auth/github'
+
+    expect(response.status).to eq(302)
+  end
+end
