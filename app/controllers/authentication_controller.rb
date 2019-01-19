0b433d7909fbe74a38702d8db3e973b26c65921e
Tim Fielder <fiel1139@gmail.com>
2019-01-19 11:05:45 -0700

    Test for tokenator.

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -11,16 +11,16 @@ class AuthenticationController < ApplicationController
     name = user_info[:name]
     avatar_url = user_info[:avatar_url]
 
-    redirect_to "#{issuer}?token=#{login + name + avatar_url}" #add header with token here.
 
     token = Tokenator.encode(login)
+    redirect_to "#{issuer}?token=#{token}" #add header with token here.
 
     User.where(login: login).first_or_create!(
       name: name,
       avatar_url: avatar_url,
       login: login
     )
-    redirect_to "#{issuer}?token=#{token}" #add header with token here.
+    #redirect_to "#{issuer}?token=#{token}" #add header with token here.
     rescue StandardError => error
     redirect_to "#{issuer}?error=#{error.message}"
   end
